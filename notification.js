0bc060ecf020b5264ce0152f9d2bf731232c46cf
Yohans Tesfaye <45452580+yohanstesfaye@users.noreply.github.com>
2022-10-01 19:36:57 +0300

    -

--- notification.js
+++ notification.js
@@ -1,20 +1,21 @@
-if ("Notification" in window) {
-    var pushNotification=function(option) {
-    if (Notification.permission === "granted") {
-        const notification = new Notification(option.title, {
-            body: option.message,
-            icon: option.icon,
-            tag: option.tag
-        });
-    } else if (Notification.permission === "denied") {
-            alert("Notification Access is denied. Grant a persmission to get a notification");    
-    } else {
-        Notification.requestPermission().then(response => {
-            pushNotification(option);         
-        });
-    }
-}
-} else {
-    alert("Your browser doesnot support push notification");
-}
 
+var pushNotification = function (option) {
+    if ("Notification" in window) {
+        if (Notification.permission === "granted") {
+            const notification = new Notification(option.title, {
+                body: option.message,
+                icon: option.icon,
+                tag: option.tag
+            });        
+        } else if (Notification.permission === "denied") {
+                alert("Notification Access is denied. Grant a persmission to get a notification");    
+        } else {
+            Notification.requestPermission().then(response => {
+                pushNotification(option);         
+            });
+        }
+    } else {
+        alert("Your browser doesnot support push notification");
+    }
+
+} 
