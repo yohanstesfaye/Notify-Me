6807ecf5ccf8c99ce942bf743e22477ca5ea4261
Yohans Tesfaye <45452580+yohanstesfaye@users.noreply.github.com>
2022-10-01 20:02:45 +0300

    comment

--- notification.js
+++ notification.js
@@ -1,26 +1,45 @@
 
 var pushNotification = function (option) {
     let image = null;
+    let silent = flase;
 
+    /**
+     * Add Image if it is passed on call
+     */
     if (option.image !== null) {
         image = option.image;
     }
+    /**
+     * set silent from option if it passed
+     */
+    if (option.silent !== null) {
+        image = option.silent;
+    }
+
+    /**
+     * Chack browser support for Notification API
+     */
     if ("Notification" in window) {
 
         if (Notification.permission === "granted") {
+            // Show Notification
             const notification=new Notification(option.title, {
                 body: option.message,
                 icon: option.icon,
                 tag: option.tag,
                 renotify: true,
+                silent: silent,
                 image: image
-            });   
+            });  
+            
             notification.addEventListener("show",e => {
                 console.log(e); 
             });
         } else if (Notification.permission === "denied") {
-                alert("Notification Access is denied. Grant a persmission to get a notification");    
+             // Alert notification access denied
+            alert("Notification Access is denied. Grant a persmission to get a notification");    
         } else {
+            // Request notification permission and show notification
             Notification.requestPermission().then(response => {
                 pushNotification(option);         
             });
