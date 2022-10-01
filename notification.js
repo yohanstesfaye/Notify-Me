4d86699cd19d2e02106b7e4c3d837ef1fdf31131
Yohans Tesfaye <45452580+yohanstesfaye@users.noreply.github.com>
2022-10-01 19:50:11 +0300

    image handler

--- notification.js
+++ notification.js
@@ -1,12 +1,23 @@
 
 var pushNotification = function (option) {
+    let image = null;
+
+    if (option.image !== null) {
+        image = option.image;
+    }
     if ("Notification" in window) {
+
         if (Notification.permission === "granted") {
-            const notification = new Notification(option.title, {
+            const notification=new Notification(option.title, {
                 body: option.message,
                 icon: option.icon,
-                tag: option.tag
-            });        
+                tag: option.tag,
+                renotify: true,
+                image: image
+            });   
+            notification.addEventListener("show",e => {
+                console.log(e); 
+            });
         } else if (Notification.permission === "denied") {
                 alert("Notification Access is denied. Grant a persmission to get a notification");    
         } else {
@@ -14,6 +25,7 @@ var pushNotification = function (option) {
                 pushNotification(option);         
             });
         }
+
     } else {
         alert("Your browser doesnot support push notification");
     }
