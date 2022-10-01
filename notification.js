390791aeab7041f29e509ef4a2d580abaeac5f84
Yohans Tesfaye <45452580+yohanstesfaye@users.noreply.github.com>
2022-10-01 16:23:29 +0300

    +

--- notification.js
+++ notification.js
@@ -1,2 +1,15 @@
 
+function pushNotification(option) {
+    let duration = 2000;
+    let title = "Notification";
+    let body = "Message is here";
 
+    Notification.requestPermission().then(response => {
+        if (response==="granted") {
+            const notification = new Notification(title, {
+                body: "This is a message body",
+                icon: "icon.png"
+            });
+       } 
+    });
+}
