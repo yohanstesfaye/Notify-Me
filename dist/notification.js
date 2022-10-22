e0be2653087557b4b5aae75e603512a202c2195c
Yohans Tesfaye <45452580+yohanstesfaye@users.noreply.github.com>
2022-10-22 14:32:46 +0300

    convert to object & add other functions too

--- dist/notification.js
+++ dist/notification.js
@@ -1,64 +1,70 @@
-var pushNotification = function (option) {
-    let image = null;
-    let silent = false;
-    let renotify = true;
+var pushNotificationObj = function () {
+    this.image = null;
+    this.silent = false;
+    this.renotify = true;
 
-    /**
-     * Add Image if it is passed on call
-     */
-    if (option.image !== null) {
-        image = option.image;
+    this.requestPermission= () => {
+        if ("Notification" in window) {
+            if (Notification.permission !== "granted" || Notification.permission !== "denied") {
+                Notification.requestPermission();
+            }
+        }
     }
-    /**
-     * set silent from option if it passed
-     */
-    if (option.silent !== null) {
-        silent = option.silent;
-    } 
-    /**
-     * set renotify from option if it passed
-     */
-    if (option.renotify !== null) {
-        renotify = option.renotify;
-    } 
 
-    /**
-     * Notification option constraction
-     */
-    notification_option = {
-        body: option.message,
-        icon: option.icon,
-        tag: option.tag,
-        renotify: renotify,
-        silent: silent,
-        image: image,
-    };
+    //notification permission
+    this.permission = () => {
+        return Notification.permission;
+    }
 
-    /**
-     * Chack browser support for Notification API
-     */
-    if ("Notification" in window) {
-
-        if (Notification.permission === "granted") {
-            // Show Notification
-            const notification=new Notification(option.title, notification_option);  
-            
-            notification.addEventListener("show",e => {
-                console.log(e); 
-            });
-        } else if (Notification.permission === "denied") {
-             // Alert notification access denied
-            alert("Notification Access is denied. Grant a persmission to get a notification");    
-        } else {
-            // Request notification permission and show notification
-            Notification.requestPermission().then(response => {
-                pushNotification(option);         
-            });
-            
+    //show notification
+    this.notify = (option) => {
+        // Add Image if it is passed on call
+        if (option.image !== null) {
+            this.image = option.image;
         }
 
-    } else {
-        alert("Your browser doesnot support push notification");
-    }
+        // set silent from option if it passed
+        if (option.silent !== null) {
+            this.silent = option.silent;
+        } 
 
-} 
+        // set renotify from option if it passed
+        if (option.renotify !== null) {
+            this.renotify = option.renotify;
+        } 
+
+        // Notification option constraction
+        notification_option = {
+            body: option.message,
+            icon: option.icon,
+            tag: option.tag,
+            renotify: this.renotify,
+            silent: this.silent,
+            image: this.image,
+        };
+
+        // Chack browser support for Notification API
+        if ("Notification" in window) {
+
+            if (Notification.permission === "granted") {
+                // Show Notification
+                const notification=new Notification(option.title, notification_option);  
+            } else if (Notification.permission === "denied") {
+                // Alert notification access denied
+                alert("Notification Access is denied. Grant a persmission to get a notification");    
+            } else {
+                // Request notification permission and show notification
+                Notification.requestPermission().then(response => {
+                    if (response == "granted") {
+                        this.notify(option); 
+                    }        
+                });
+            }
+
+        } else {
+            alert("Your browser doesnot support push notification");
+        }
+   }
+}
+
+var pushNotification = new pushNotificationObj();
--- readme.md
+++ readme.md
@@ -1,20 +1,42 @@
 # Push Notification
 
 A simple javascript push notification library for a websites.
-# How to use
+
+## How to use
+
 to add a push notification to your website add
 
 1. Add/lonk javascript file from **dist** folder to your website
 2. make a function call to on javascript
 
+
+### Request Notification Permission
+
+```js
+pushNotification.requestPermission();
+```
+
+### Get Notification Permission
+get the status of website notification permission. return values are **default**, **granted** and **denied**
+
+```js
+
+pushNotification.permission();
+```
+
+### Show notification
+
+to make a push notification make a call to member function **notify** it will have an object argument **option** which have 3 important properties **title**, **tag**, **message** other more optional properties.
+
 ``` js
-pushNotification({
+pushNotification.notify({
     title: "Title",
     tag: "tag",
     message: "Notification body/message",
 });
 ```
-# Parameters
+
+## Parameters
 
 | Parameter | description | value | Default | Note|
 |----|------|----|----|------ |
@@ -22,6 +44,6 @@ pushNotification({
 |**tag**|linking morethan one notification together| _string_ | _null_|Important|
 |__message__|notification body| _string_ | _null_|Important|
 |__icon__|url address to icon/logo|_url_|_null_||
-|**image**|url address to image|_url_|_null_|not supported by all browsers|
-|**silent**|intended to fire a silent notification|_boolean_|_false_|not supported by all browsers|
-|**renotify**|intended to fire a silent notification|_boolean_|_true_|not supported by all browsers|
+|**image**|url address to image|_url_|_null_|[not supported by all browsers](https://developer.mozilla.org/en-US/docs/Web/API/Notification/image#browser_compatibility)|
+|**silent**|intended to fire a silent notification|_boolean_|_false_|[not supported by all browsers](https://developer.mozilla.org/en-US/docs/Web/API/Notification/silent#browser_compatibility)|
+|**renotify**|intended to fire a silent notification|_boolean_|_true_|[not supported by all browsers](https://developer.mozilla.org/en-US/docs/Web/API/Notification/renotify#browser_compatibility)|
